6819dcac73d395ead621ef4b98218ed61e5b61b9
Munindar Reddy <munni.ch12@gmail.com>
2019-07-20 21:07:28 +0530

    Changed the file separator to generic and updated language level to 11

--- src/test/java/Base/BaseUtil.java
+++ src/test/java/Base/BaseUtil.java
@@ -4,6 +4,8 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+
 /**
  * Created by Karthik on 31/01/2019.
  */
@@ -18,6 +20,9 @@ public class BaseUtil {
 
     public static ExtentTest features;
 
-    public static String reportLocation = "/Users/karthikkk/report/";
+    public static String filePathSeparator = System.getenv(File.pathSeparator);
+
+    public static String reportLocation = filePathSeparator+"Users"+filePathSeparator+"karthikkk"+filePathSeparator+"report"+filePathSeparator;
+
 
 }
--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -31,7 +31,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\Libs\chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:"+filePathSeparator+"Libs"+filePathSeparator+"chromedriver");
         base.Driver = new ChromeDriver();
     }
 
